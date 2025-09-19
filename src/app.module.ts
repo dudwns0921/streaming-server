21a4a71e28b6b05838e217f4b44880986cd6632b
elio.Jung <elio.Jung@obigo.com>
2025-09-19 16:54:48 +0900

    fix: 누락된 코드 원복

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideoModule } from './video/video.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -16,5 +18,7 @@ import { VideoModule } from './video/video.module';
     }),
     VideoModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
